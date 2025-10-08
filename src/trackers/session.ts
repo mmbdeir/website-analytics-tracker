e888eb50e394deb3498a366fd6b74f1ac93b8e16
Mohamad <moeman.bdeir09@gmail.com>
2025-10-07 21:47:13 -0400

    Now tracks sessions using localstorage based on time passes afk and time passed between windows and tabs

--- src/trackers/session.ts
+++ src/trackers/session.ts
@@ -1,21 +1,41 @@
+import { throttle } from "../reusables/throttle";
+
 let timer: NodeJS.Timeout;
 
-let time = 3 * 1000;
+let time = 1000 * 60 * 30;
 
-function endSession() {
-  console.log("Session is done due to inactivity");
+function sessionPrint() {
+  console.log("A new session");
 }
 
+function og() {
+  let ts = Date.now() - Number(localStorage.getItem("at-sess-timestamp"));
+  if (ts > time) {
+    localStorage.setItem("at-sess-timestamp", Date.now().toString());
+    sessionPrint();
+    console.log("hi");
+  } else {
+    localStorage.setItem("at-sess-timestamp", Date.now().toString());
+    console.log("hiiiiiiiiii");
+  }
+}
+
+let cs = throttle(function () {
+  localStorage.setItem("at-sess-timestamp", Date.now().toString());
+}, 3000);
+
 function resetTimer() {
-  clearTimeout(timer);
-  timer = setTimeout(endSession, time);
+  if (timer) clearTimeout(timer);
+  timer = setTimeout(sessionPrint, time);
+  cs();
 }
 
 export function session() {
-  window.onscroll = resetTimer;
+  og();
   window.onclick = resetTimer;
+  window.onscroll = resetTimer;
+  window.onkeydown = resetTimer;
   window.onmousemove = resetTimer;
   window.onload = resetTimer;
-  window.onkeydown = resetTimer;
   window.ontouchstart = resetTimer;
 }
