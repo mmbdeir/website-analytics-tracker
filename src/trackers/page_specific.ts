d5dd39a11792e78edf6aa8c12dc9fff0b5259286
Mohamad <moeman.bdeir09@gmail.com>
2025-11-30 23:56:22 -0500

    Made everything work

--- src/trackers/page_specific.ts
+++ src/trackers/page_specific.ts
@@ -3,54 +3,39 @@ import "scrollyfills";
 const CURRENT_SESSION_START_TIME = "current_session_start_time";
 
 export class PageSpecific {
-  private static initilized = false;
+  private static initialized = false;
+  private static navPaths: string[] = [];
+  static getMaxScrollDepth: () => number;
+
   static init() {
-    if (this.initilized) return;
-    this.initilized = true;
-    const navPaths = this.initNavPaths();
-    const getMaxDepth = this.initScrollDepth();
-    this.initVisibilityHandler(navPaths, getMaxDepth);
-    this.timePerPage();
+    if (this.initialized) return;
+    this.initialized = true;
+    this.getMaxScrollDepth = this.initScrollDepth();
+    this.navPaths = this.initNavPaths();
+    this.initVisibilityHandler(this.getMaxScrollDepth);
   }
   // Amount of sessions per page
   // - on page open(not reload) a new page session with the pages name from the metadata, and its url
   // From what page this user came from to get to this page
-  private static initVisibilityHandler(
-    navPaths: string[],
-    getMaxScrollDepth: () => number
-  ) {
+  private static initVisibilityHandler(getMaxScrollDepth: () => number) {
     document.addEventListener("visibilitychange", (e) => {
-      if (document.visibilityState === "hidden") {
-        const start = Number(localStorage.getItem(CURRENT_SESSION_START_TIME));
-        const duration = Date.now() - start;
-        navigator.sendBeacon(
-          "Use cloud run url to host the function, im using sendBeacon so it still runs if the tab closes",
-          JSON.stringify({
-            pageLeft: window.location.pathname,
-          })
-        );
-        navigator.sendBeacon(
-          "end point",
-          JSON.stringify({ duration: duration })
-        );
-        navigator.sendBeacon(
-          "endpoint",
-          JSON.stringify({ navPaths: navPaths })
-        );
-        navigator.sendBeacon(
-          "endpoint",
-          JSON.stringify({ scrollDepth: getMaxScrollDepth() })
-        );
-      }
+      if (document.visibilityState !== "hidden") return;
+      const start = Number(localStorage.getItem(CURRENT_SESSION_START_TIME));
+      const duration = Date.now() - start;
+      navigator.sendBeacon(
+        "end point",
+        JSON.stringify({
+          pageDuration: duration,
+          navPaths: this.navPaths,
+          pageLeft: window.location.pathname,
+          scrollDepth: getMaxScrollDepth(),
+        })
+      );
     });
-  }
-
-  private static timePerPage() {
     localStorage.setItem(CURRENT_SESSION_START_TIME, Date.now().toString());
   }
 
   // Track what page the user came from and where did he go after
-
   private static initNavPaths(): string[] {
     const navPaths = [window.location.pathname];
     const pushNavPaths = () => {
@@ -80,22 +65,38 @@ export class PageSpecific {
         maxDepth = currentDepth;
       }
     });
+    window.addEventListener("resize", () => {
+      clientHeight = document.documentElement.clientHeight;
+    });
     return () => maxDepth;
   }
 }
 
 function navigationChange(callback: () => void) {
-  window.addEventListener("popstate", callback);
+  const run = () => {
+    const start = Number(localStorage.getItem(CURRENT_SESSION_START_TIME));
+    const duration = Date.now() - start;
+    navigator.sendBeacon(
+      "ENDPOINT",
+      JSON.stringify({
+        pageDuration: duration,
+        scrollDetph: PageSpecific.getMaxScrollDepth(),
+      })
+    );
+    localStorage.setItem(CURRENT_SESSION_START_TIME, Date.now().toString());
+  };
 
   const originalPushState = history.pushState;
   history.pushState = function (...args) {
     originalPushState.apply(history, args);
+    run();
     callback();
   };
 
   const originalReplaceState = history.replaceState;
   history.replaceState = function (...args) {
     originalReplaceState.apply(history, args);
+    run();
     callback();
   };
 }
